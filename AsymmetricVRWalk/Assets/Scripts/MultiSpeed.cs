067cf2312068bfd653eb9ea502a303f3516b7ad9
DerpyMongoose <nicolajevers@hotmail.com>
2017-03-24 12:23:52 +0100

    This is commit
    Blagh

---
+++ AsymmetricVRWalk/Assets/Scripts/MultiSpeed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiSpeed : MonoBehaviour
+{
+    [HideInInspector]
+    public float transitionLengthX;
+    [HideInInspector]
+    public float transitionLengthY;
+    [HideInInspector]
+    public float transitionLengthZ;
+    [HideInInspector]
+    public float speed;
+
+    private bool toggle;
+
+    void Start()
+    {
+        transitionLengthX = GetComponentInParent<triggerController>().transitionLengthX;
+        transitionLengthY = GetComponentInParent<triggerController>().transitionLengthY;
+        transitionLengthZ = GetComponentInParent<triggerController>().transitionLengthZ;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+            GetComponentInParent<triggerController>().SwitchTrig();
+    }
+}
