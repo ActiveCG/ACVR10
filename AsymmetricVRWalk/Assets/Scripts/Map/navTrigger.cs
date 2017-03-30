77214a0eb1d9e52caef02661306d1b89e2096122
DerpyMongoose <nicolajevers@hotmail.com>
2017-03-30 04:45:09 +0200

    Fixed the map transitions
    If you can't fix a problem smartly just throw cpu at it until it
    works... I swear it's not as bad as it seems... ... :)

---
+++ AsymmetricVRWalk/Assets/Scripts/Map/navTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navTrigger : MonoBehaviour {
+
+    public float x;
+    public float y;
+    public float z;
+
+    private bool yee_ol_switcheroo = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Mimic")
+        {
+            if (yee_ol_switcheroo == false)
+            {
+                other.GetComponent<Navigation>().UpdateOffset(x, y, z);
+            }
+            else if (yee_ol_switcheroo == true)
+            {
+                other.GetComponent<Navigation>().UpdateOffset(-x, -y, -z);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Mimic")
+        {
+            yee_ol_switcheroo = !yee_ol_switcheroo;
+        }
+    }
+}
