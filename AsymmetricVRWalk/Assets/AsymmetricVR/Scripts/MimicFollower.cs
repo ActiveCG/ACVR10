00314e2423f067297393f7a8d70509f3a87037ab
activeaudio <sivers12@student.aau.dk>
2017-04-01 23:08:05 +0200

    * map prologation + mimic follower

--- AsymmetricVRWalk/Assets/AsymmetricVR/Scripts/MimicFollower.cs
+++ AsymmetricVRWalk/Assets/AsymmetricVR/Scripts/MimicFollower.cs
@@ -14,11 +14,13 @@ public class MimicFollower : MonoBehaviour {
 	private float prolongStart;//in map
 	private float prolongEnd;
 	private Vector3 prolongOffset;
+	private bool inTunnelEntrance;//yo allow entrance in a tunnel
 
 	void Start () {
 		hmd = GameObject.FindGameObjectWithTag ("Player").transform;
 		currentProlong = null;
 		prolongOffset = Vector3.zero;
+		inTunnelEntrance = false;
 	}
 
 	void Update () {
@@ -26,6 +28,8 @@ public class MimicFollower : MonoBehaviour {
 			SpeedUp ();
 		} else {
 			transform.position = refPoint.position + hmd.position + prolongOffset;
+			float y = Mathf.Clamp (hmd.transform.position.y, 0.5f, 2.6f);
+			transform.position = new Vector3 (transform.position.x, y, transform.position.z);
 		}
 
 		transform.rotation = hmd.rotation;
@@ -33,7 +37,7 @@ public class MimicFollower : MonoBehaviour {
 
 	private void SpeedUp(){
 		Vector3 offset = Vector3.zero;
-		if (currentProlong.x_plus > 0) {
+		if (currentProlong.prolongedX > 0) {
 			float p = (hmd.position.x - tileStart) / (tileEnd - tileStart);
 			float extra = 0f;
 			if (p < 0f) {
@@ -49,7 +53,7 @@ public class MimicFollower : MonoBehaviour {
 			}
 			offset = new Vector3 (x_offset, hmd.position.y, hmd.position.z);
 		}
-		else if (currentProlong.z_plus > 0) {
+		else if (currentProlong.prolongedZ > 0) {
 			float p = (hmd.position.z - tileStart) / (tileEnd - tileStart);
 			float extra = 0f;
 			if (p < 0f) {
@@ -66,37 +70,56 @@ public class MimicFollower : MonoBehaviour {
 			offset = new Vector3 (hmd.position.x, hmd.position.y, z_offset);
 		}
 		transform.position = refPoint.position + prolongOffset + offset;
+		float y = hmd.transform.position.y;
+		if (currentProlong.type == ProlongationType.TUNNEL) {
+			y = Mathf.Clamp (y, 0f, 0.3f);
+		} else {
+			y = Mathf.Clamp (y, 0.5f, 2.6f);
+		}
+		transform.position = new Vector3 (transform.position.x, y, transform.position.z);
 	}
 
 	void OnTriggerEnter(Collider col){
+		if (col.tag == "TunnelEntrance") {
+			inTunnelEntrance = true;
+		}
 		if (col.GetComponent<Prolongation> () != null && currentProlong == null) {
-			currentProlong = col.GetComponent<Prolongation> ();
-			tileStart = currentProlong.x_start;
-			tileEnd = currentProlong.x_end;
-			prolongStart = currentProlong.x_prolongStart;
-			prolongEnd = currentProlong.x_prolongEnd;
-			print ("in");
+			if ((col.GetComponent<Prolongation> ().type == ProlongationType.TUNNEL && inTunnelEntrance == true)
+			   || col.GetComponent<Prolongation> ().type == ProlongationType.BRIDGE) {
+				currentProlong = col.GetComponent<Prolongation> ();
+				tileStart = currentProlong.tileStart;
+				tileEnd = currentProlong.tileEnd;
+				prolongStart = currentProlong.prolongationStart;
+				prolongEnd = currentProlong.prolongationEnd;
+				print ("in");
+			}
 		}
 	}
 
 	void OnTriggerStay(Collider col){
 		if (col.GetComponent<Prolongation> () != null && currentProlong == null) {
-			currentProlong = col.GetComponent<Prolongation> ();
-			tileStart = currentProlong.x_start;
-			tileEnd = currentProlong.x_end;
-			prolongStart = currentProlong.x_prolongStart;
-			prolongEnd = currentProlong.x_prolongEnd;
-			print ("in");
+			if ((col.GetComponent<Prolongation> ().type == ProlongationType.TUNNEL && inTunnelEntrance == true)
+			    || col.GetComponent<Prolongation> ().type == ProlongationType.BRIDGE) {
+				currentProlong = col.GetComponent<Prolongation> ();
+				tileStart = currentProlong.tileStart;
+				tileEnd = currentProlong.tileEnd;
+				prolongStart = currentProlong.prolongationStart;
+				prolongEnd = currentProlong.prolongationEnd;
+				print ("in");
+			}
 		}
 	}
 
 	void OnTriggerExit(Collider col){
+		if (col.tag == "TunnelEntrance") {
+			inTunnelEntrance = false;
+		}
 		if (col.GetComponent<Prolongation> () != null && col.GetComponent<Prolongation>() == currentProlong) {
-			if (currentProlong.x_plus > 0) {
+			if (currentProlong.prolongedX > 0) {
 				float p = (hmd.position.x - tileStart) / (tileEnd - tileStart);
 				p = Mathf.Clamp01 (p);
 				print ("exit " + p);
-				float x_offset = 1f * (prolongEnd - prolongStart) * currentProlong.x_plus / (currentProlong.x_plus + 1f);
+				float x_offset = 1f * (prolongEnd - prolongStart) * currentProlong.prolongedX / (currentProlong.prolongedX + 1f);
 				if (p == 0f && currentProlong.addedToOffset == true) {
 					prolongOffset = new Vector3 (prolongOffset.x - x_offset, prolongOffset.y, prolongOffset.z);
 					currentProlong.addedToOffset = false;
@@ -105,11 +128,11 @@ public class MimicFollower : MonoBehaviour {
 					currentProlong.addedToOffset = true;
 				}
 			}
-			else if (currentProlong.z_plus > 0) {
+			else if (currentProlong.prolongedZ > 0) {
 				float p = (hmd.position.z - tileStart) / (tileEnd - tileStart);
 				p = Mathf.Clamp01 (p);
 				print ("exit " + p);
-				float z_offset = 1f * (prolongEnd - prolongStart) * currentProlong.z_plus / (currentProlong.z_plus + 1f);
+				float z_offset = 1f * (prolongEnd - prolongStart) * currentProlong.prolongedZ / (currentProlong.prolongedZ + 1f);
 				if (p == 0f && currentProlong.addedToOffset == true) {
 					prolongOffset = new Vector3 (prolongOffset.x, prolongOffset.y, prolongOffset.z - z_offset);
 					currentProlong.addedToOffset = false;
--- AsymmetricVRWalk/Assets/AsymmetricVR/Scripts/Prolongation.cs
+++ AsymmetricVRWalk/Assets/AsymmetricVR/Scripts/Prolongation.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Prolongation : MonoBehaviour {
+public enum ProlongationType{
+	BRIDGE, TUNNEL
+}
 
-	public float x_plus;
-	public float z_plus;
-	public float x_start;
-	public float x_end;
-	public float x_prolongStart;
-	public float x_prolongEnd;
+public class Prolongation : MonoBehaviour {
+	
+	public float prolongedX; //how many tiles to add extra to the one in maze
+	public float prolongedZ;
+	public float tileStart;//in maze
+	public float tileEnd;//in maze
+	public float prolongationStart;//in map
+	public float prolongationEnd;//in map
+	public ProlongationType type;
 	[HideInInspector]
 	public bool addedToOffset;
 
