a700887b5b2be5e94826df1efae2b824968edc9c
DerpyMongoose <nicolajevers@hotmail.com>
2017-04-20 19:55:41 +0200

    Timer smiley things whatever temp desktop transfer

---
+++ AsymmetricVRWalk/Assets/XUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class XUIManager : MonoBehaviour {
+
+    string fileName = "MyFile.txt";
+
+    public void OnButtonPress()
+    {
+        if (File.Exists(fileName))
+        {
+            Debug.Log(fileName + " already exists.");
+            return;
+        }
+        var sr = File.CreateText(fileName);
+        sr.WriteLine("This is my file.");
+        sr.WriteLine("I can write ints {0} or floats {1}, and so on.",
+            1, 4.2);
+        sr.Close();
+    }
+}
