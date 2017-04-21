d87d534f0cd32ef9239c31266c4dbee213d4e3dd
Nicolaj Evers <nicolajevers@hotmail.com>
2017-04-21 22:51:38 +0200

    -_o_- everything works

---
+++ AsymmetricVRWalk/Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown("w"))
+        {
+            transform.position -= new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(0, 0, 0.1f);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            transform.position -= new Vector3(0, 0, 0.1f);
+        }
+    }
+}
